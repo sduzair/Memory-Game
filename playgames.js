be51d20f3ed1cccf1e6f59f2edbb4c0d5c89ac67
sduzair <sduzairahmed@gmail.com>
2022-03-19 05:08:32 -0400

    removed confetti at start

--- playgames.js
+++ playgames.js
@@ -119,12 +119,6 @@ $(() => {
     // reseting incase of abrupt game finish
     structure.correctSelections = 0;
     structure.incorrectSelections = 0;
-    // confetty congrgulations
-    $.confetti.start();
-    setTimeout(() => {
-      $.confetti.stop();
-    }, 1500);
-    $("#play_game").trigger("click");
     console.log("current player: ", sessionStorage.currentPlayerName);
 
     game.createGrid($("#tabs-1"), $("#cards"), game.getImageIds());
